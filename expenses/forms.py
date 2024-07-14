9012f2f505db291d60f0db9fd73769ecae733267
Houman Hafez <houman.hafezalghoran@gmail.com>
2024-07-15 01:07:03 +0200

    Feature Addtion: Search and Chart.js (#4)
    * removed add_month. Added chart.js, Updated the look of the Web App, Added add_month functionality to the add_expense and edit_expense
    
    * .
    
    * Added Search Options, Added Chart.js,
    Added Other functionalities for the web app

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Expense, Month
+from .models import Expense
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ['date', 'category', 'amount', 'currency']
+        widgets = {
+            'currency': forms.TextInput(attrs={'placeholder': 'e.g., USD, EUR'}),
+        }
+        initial = {
+            'currency': 'EUR',
+        }
--- expenses/models.py
+++ expenses/models.py
@@ -17,8 +17,7 @@ class Expense(models.Model):
     category = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     month = models.ForeignKey(Month, related_name='expenses', on_delete=models.CASCADE)
-    currency = models.CharField(max_length=3, default='USD') 
-
+    currency = models.CharField(max_length=3, default='EUR', blank=True, null=True)
 
     def __str__(self):
-        return f'{self.date} - {self.category} - ${self.amount}'
+        return f"{self.category} - {self.amount} {self.currency or 'EUR'}"
