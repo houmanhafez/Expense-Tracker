8278ff6b28270d0bc9ecc1ac35827d200738abae
Houman Hafez <houman.hafezalghoran@gmail.com>
2024-07-14 15:54:04 +0200

    formatted all the files

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from .models import Expense, Month
 
+
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
-        fields = ['date', 'category', 'amount', 'month']
+        fields = ["date", "category", "amount", "month"]
+
 
 class MonthForm(forms.ModelForm):
     class Meta:
         model = Month
-        fields = ['name']
+        fields = ["name"]
