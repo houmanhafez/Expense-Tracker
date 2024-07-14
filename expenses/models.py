286845a6777d398282b551ca09e5921fba3431ca
Houman Hafez <houman.hafezalghoran@gmail.com>
2024-07-14 03:51:05 +0200

    removed add_month. Added chart.js, Updated the look of the Web App, Added add_month functionality to the add_expense and edit_expense

--- expenses/models.py
+++ expenses/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Month(models.Model):
     name = models.CharField(max_length=50)
 
+    @property
+    def total_expenses(self):
+        return self.expenses.aggregate(total=models.Sum('amount'))['total'] or 0
+
     def __str__(self):
         return self.name
 
@@ -10,7 +14,9 @@ class Expense(models.Model):
     date = models.DateField()
     category = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    month = models.ForeignKey(Month, on_delete=models.CASCADE, related_name='expenses')
+    month = models.ForeignKey(Month, related_name='expenses', on_delete=models.CASCADE)
+    currency = models.CharField(max_length=3, default='USD') 
+
 
     def __str__(self):
-        return f"{self.date} - {self.category} - {self.amount}"
+        return f'{self.date} - {self.category} - ${self.amount}'
--- expenses/views.py
+++ expenses/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 from .models import Expense, Month
-from .forms import ExpenseForm, MonthForm
+from .forms import ExpenseForm
+from django.utils.dateparse import parse_date
+
 
 def index(request):
     months = Month.objects.all()
-    total_expenses = {month: sum(expense.amount for expense in month.expenses.all()) for month in months}
-    return render(request, 'expenses/index.html', {'months': months, 'total_expenses': total_expenses})
+    return render(request, 'expenses/index.html', {'months': months})
 
 def add_expense(request):
     if request.method == 'POST':
         form = ExpenseForm(request.POST)
         if form.is_valid():
-            form.save()
+            expense = form.save(commit=False)
+            expense_date = form.cleaned_data['date']
+            month_name = expense_date.strftime('%B')
+            month, created = Month.objects.get_or_create(name=month_name)
+            expense.month = month
+            expense.save()
             return redirect('index')
     else:
         form = ExpenseForm()
+    
     return render(request, 'expenses/add_expense.html', {'form': form})
-
+    return render(request, 'expenses/add_expense.html', {'form': form})
 def add_month(request):
     if request.method == 'POST':
         form = MonthForm(request.POST)
@@ -27,8 +35,9 @@ def add_month(request):
         form = MonthForm()
     return render(request, 'expenses/add_month.html', {'form': form})
 
-def edit_expense(request, pk):
-    expense = get_object_or_404(Expense, pk=pk)
+def edit_expense(request, expense_id):
+    expense = get_object_or_404(Expense, id=expense_id)
+    
     if request.method == 'POST':
         form = ExpenseForm(request.POST, instance=expense)
         if form.is_valid():
@@ -36,4 +45,13 @@ def edit_expense(request, pk):
             return redirect('index')
     else:
         form = ExpenseForm(instance=expense)
+    
     return render(request, 'expenses/edit_expense.html', {'form': form, 'expense': expense})
+
+def expense_data(request):
+    expenses = Expense.objects.all()
+    data = {
+        'labels': [expense.date for expense in expenses],
+        'data': [expense.amount for expense in expenses],
+    }
+    return JsonResponse(data)
